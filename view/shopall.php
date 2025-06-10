e5714c91a7079015731705de392ceaa1477f3d5c
anmol597 cyberrr <anmoldeepbhullar96@gmail.com>
2025-06-09 22:44:41 -0400

    shop all  file

--- view/shopall.php
+++ view/shopall.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Best Perfumes Collection</title>
+    <title>All Perfumes Collection</title>
     <style>
         * {
             box-sizing: border-box;
